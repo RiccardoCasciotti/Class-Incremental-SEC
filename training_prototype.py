d6d1d89232442d71f86f22bf9ac9f38f24d51844
mparta <185134659+mparta@users.noreply.github.com>
2025-06-23 19:03:36 +0300

    Restructuring the training_prototype. Added the evaluation. Part of the argparsing. Added rudimentary checkpointing. Choosing the best model based on validation loss. Added basic metrics.

--- training_prototype.py
+++ training_prototype.py
@@ -1,76 +1,17 @@
 import copy
 import time
+import argparse
 
 import torch
 import torchaudio
 import torch.nn as nn
 import torch.optim as optim
-
+from sklearn.metrics import classification_report, average_precision_score, f1_score
 import numpy as np
+from collections import Counter
 
 from cnn14_pann_lin import Cnn14
 from cl_dataset_class import CL_dataset
-
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-
-print(f"Using device: {device}")
-
-nr_of_classes = 30
-
-PATH_TO_HDF5_DATA = r'C:\Users\mp431591\Documents\work_code\cl_30\continual_learning\data_cl.hdf5'
-
-audioset_data_train = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
-                           dataset='audioset',
-                           split='train',
-                           nr_of_classes=30)
-
-audioset_data_eval = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
-                                dataset='audioset',
-                                split='eval',
-                                nr_of_classes=30)
-print(len(audioset_data_train))
-print(len(audioset_data_eval))
-
-train_data, val_data = torch.utils.data.random_split(audioset_data_train, [0.9, 0.1])
-smaller_train_data, _, smaller_val_data  = torch.utils.data.random_split(val_data, [0.01, 0.98, 0.01])
-print(len(train_data))
-print(len(val_data))
-
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=8)
-validation_loader = torch.utils.data.DataLoader(val_data, batch_size=8)
-evaluation_loader = torch.utils.data.DataLoader(audioset_data_eval, batch_size=8)
-
-small_train_loader = torch.utils.data.DataLoader(smaller_train_data, batch_size=8)
-small_val_loader = torch.utils.data.DataLoader(smaller_val_data, batch_size=8)
-
-test_mel, test_label, _ = next(iter(evaluation_loader))
-print(test_mel.shape)
-print(test_label.shape)
-
-# From CIL-ML-AUDIO
-lr = 0.1
-lr_min = 0.0001 
-epochs = 10
-momentum = 0.9
-weight_decay = 5e-4
-
-model = Cnn14(nr_of_classes)
-model = model.to(device)
-
-pos_weight = audioset_data_train.get_pos_weight()
-print(f"Pos weight type: {type(pos_weight)}")
-
-loss_fn = nn.BCEWithLogitsLoss() # Use of pos_weight?
-loss_fn_weighted = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-
-# Use of weight decay copied from Manju's script
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum,
-                      weight_decay=weight_decay) 
-
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=lr_min)
 
 # Code modified from Pytorch's quickstart tutorial
 def train(dataloader, model, loss_fn, optimizer, scheduler):
@@ -98,7 +39,6 @@ def train(dataloader, model, loss_fn, optimizer, scheduler):
         end_time = time.time()
         running_time += end_time - start_time
         iterations += 1
-
         
         if batch % 2 == 0:
             print(f"Average batch training time: {running_time / iterations}")
@@ -124,18 +64,172 @@ def validate(dataloader, model, loss_fn):
     print(f"Avg loss through validation: {val_loss:>8f} \n")
     return val_loss
 
+# Following CIL-ML-AUDIO's example
+def evaluate(model, eval_loader, model_state_dict=None):
+    if model_state_dict != None:
+        model.load_state_dict(model_state_dict)
+    model.eval()
+
+    all_preds = []
+    all_targets = []
+
+    with torch.no_grad():
+        for mel, label, fname in eval_loader:
+            out, _ = model(mel.float())
+            preds = torch.gt(torch.sigmoid(out), 0.5)
+            all_preds.extend(
+                preds.cpu().numpy())
+            all_targets.extend(np.asarray(label))
+
+        Y_predicted = np.asarray(all_preds)
+        Y_ref = np.asarray(all_targets)
+
+        print(f"Predicted array: {Y_predicted} and its type {type(Y_predicted)}")
+        print(f"Ground truth array: {Y_ref} and its type {type(Y_ref)}")
+
+
+        print('Reference polyphony:', Counter(Y_ref.sum(axis=1)))
+        print('Predicted polyphony:', Counter(Y_predicted.sum(axis=1)))
+        print(classification_report(Y_ref, Y_predicted))
+
+        average_precision = average_precision_score(Y_ref, Y_predicted, average=None)
+        mAp = np.mean(average_precision)
+        print('mAP', mAp)
+
+        f1_macro = f1_score(Y_ref, Y_predicted, average='macro', zero_division=0.0)
+        f1_micro = f1_score(Y_ref, Y_predicted, average='micro', zero_division=0.0)
+        print('macro', f1_macro)
+        print('micro', f1_micro)
+
+        return mAp, f1_macro, f1_macro
+
+
+
 
 if __name__ == '__main__':
 
+    # Command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--epochs', type=int, default=10, help='Number of epochs to train')
+    parser.add_argument('-nr', '--nr_of_classes', type=int, default=30, help='Number of classes to use from data')
+    parser.add_argument('-d', '--dataset', type=str, default='audioset', help='Choice of dataset. Either audioset or fsd50k')
+    parser.add_argument('-sp', '--split', type=str, default='train', help='Dataset split. Either train or eval')
+    parser.add_argument('-pd', '--path_to_data', type=str, help='The path to the HDF5 datafile.')
+    parser.add_argument('-nr_w', '--nr_of_workers', type=int, default=1, help='Number of workers for dataloading')
+    args = vars(parser.parse_args())
+
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    print(f"Using device: {device}")
+
+    # Dataloading
+    nr_of_classes = 30
+    dataset = 'audioset'
+    PATH_TO_HDF5_DATA = r'C:\Users\mp431591\Documents\work_code\cl_30\continual_learning\data_cl.hdf5'
+
+    data_train = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
+                            dataset=dataset,
+                            split='train',
+                            nr_of_classes=30)
+
+    data_eval = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
+                                    dataset='audioset',
+                                    split='eval',
+                                    nr_of_classes=30)
+    print(len(data_train))
+    print(len(data_eval))
+
+    train_data, val_data = torch.utils.data.random_split(data_train, [0.9, 0.1])
+    smaller_train_data, _, smaller_val_data  = torch.utils.data.random_split(val_data, [0.01, 0.98, 0.01])
+    print(len(train_data))
+    print(len(val_data))
+
+    small_eval_data, _ = torch.utils.data.random_split(data_eval,
+                                                    [0.001, 0.999])
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=8)
+    validation_loader = torch.utils.data.DataLoader(val_data, batch_size=8)
+    evaluation_loader = torch.utils.data.DataLoader(data_eval, batch_size=8)
+
+    small_train_loader = torch.utils.data.DataLoader(smaller_train_data, batch_size=8)
+    small_val_loader = torch.utils.data.DataLoader(smaller_val_data, batch_size=8)
+    small_eval_loader = torch.utils.data.DataLoader(small_eval_data, batch_size=8)
+
+    test_mel, test_label, _ = next(iter(evaluation_loader))
+    print(test_mel.shape)
+    print(test_label.shape)
+
+    # From CIL-ML-AUDIO
+    lr = 0.1
+    lr_min = 0.0001 
+    epochs = 10
+    momentum = 0.9
+    weight_decay = 5e-4
+
+    model = Cnn14(nr_of_classes)
+    model = model.to(device)
+
+    pos_weight = data_train.get_pos_weight()
+    print(f"Pos weight type: {type(pos_weight)}")
+
+    loss_fn = nn.BCEWithLogitsLoss() # Use of pos_weight?
+    loss_fn_weighted = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+
+    # Use of weight decay copied from Manju's script
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum,
+                        weight_decay=weight_decay) 
+
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=lr_min)
+
+    val_loss = 0
+    best_val_loss = float('inf')
     old_val_loss = 0 # Using val_loss as early stopping condition
     val_loss_thr = 0.0001
+    patience_counter = 0
+    patience_thr = 5
 
-    for i in range(epochs):
+    best_model_state = {}
+
+
+    for epoch in range(epochs):
+
+        # Training
         train(dataloader=small_train_loader, model=model,
         loss_fn=loss_fn_weighted, optimizer=optimizer,
         scheduler=scheduler)
-        val_loss = validate(dataloader=small_val_loader, model=model,loss_fn=loss_fn)
 
+        # Validation
+        val_loss = validate(dataloader=small_val_loader, model=model,loss_fn=loss_fn_weighted)
+
+        # Check for patience and early stopping
         if abs(old_val_loss - val_loss) < val_loss_thr:
-            print(f"Validation loss had a change smaller than 0.{val_loss_thr}. Stopping early.")
-            break
+            patience_counter += 1
+            if patience_counter >= patience_thr:
+                print(f"Validation loss had a change smaller than {val_loss_thr} {patience_thr} times. Stopping early.")
+                break
+        old_val_loss = val_loss
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            # Save model
+            best_model_state = copy.deepcopy(model.state_dict())
+
+        # Save the scheduler, optimizer, and model state periodically
+        # Inspired partly by https://debuggercafe.com/saving-and-loading-the-best-model-in-pytorch/
+        if epoch % 5 == 0:
+            print("Saving training state from epoch {epoch}.")
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict()
+            }, 'latest_chkp_dict.pth')
+
+    
+    evaluate(model=model, eval_loader=small_eval_loader, model_state_dict=best_model_state)
+
+    # Save the model for inference
+    model_name = 'trained_model_' + dataset + '_' + str(nr_of_classes) + '.pt'
+    torch.save(best_model_state, model_name)
