daccb35d1a6aa6580a346a2df145b821c785034d
mparta <185134659+mparta@users.noreply.github.com>
2025-07-22 16:48:09 +0300

    Made a simpler printing of the initial 30 classes results. Removed all logic for comparing with another older model.

--- prototype_cil_evaluation.py
+++ prototype_cil_evaluation.py
@@ -8,6 +8,8 @@ from collections import Counter
 from cnn14_pann_lin import Cnn14
 from cl_dataset_class import CL_dataset
 
+# Overall heavily copied from and inspired by CIL-ML-AUDIO
+
 def evaluate(model, eval_loader):
 
     all_preds = []
@@ -26,12 +28,12 @@ def evaluate(model, eval_loader):
         Y_predicted = np.asarray(all_preds)
         Y_ref = np.asarray(all_targets)
 
+        initial_preds = Y_predicted[:, 0:30] # 30 classes originally
+        initial_labels = Y_ref[:, 0:30]
+
         #print(f"Predicted array: {Y_predicted} and its type {type(Y_predicted)}")
         #print(f"Ground truth array: {Y_ref} and its type {type(Y_ref)}")
 
-
-        print('Reference polyphony:', Counter(Y_ref.sum(axis=1)))
-        print('Predicted polyphony:', Counter(Y_predicted.sum(axis=1)))
         report = classification_report(Y_ref, Y_predicted, output_dict=True)
         print(classification_report(Y_ref, Y_predicted))
 
@@ -44,35 +46,40 @@ def evaluate(model, eval_loader):
         print('macro', f1_macro)
         print('micro', f1_micro)
 
+        # Values for the original 30 classes
+        
+        report_ini = classification_report(initial_labels,
+                                           initial_preds, 
+                                           output_dict=True)
+        print("For the initial 30 classes", flush=True)
+        print(classification_report(initial_labels, initial_preds))
+
+        avg_prec_init = average_precision_score(initial_labels,
+                                                initial_preds,
+                                                average=None)
+
+        mAp_init = np.mean(avg_prec_init)
+        print(f'Initial 30 classes mAP: {mAp_init}')
+
+        f1_macro_init = f1_score(Y_ref, Y_predicted, average='macro', zero_division=0.0)
+        f1_micro_init = f1_score(Y_ref, Y_predicted, average='micro', zero_division=0.0)
+        print(f'Initial 30 classes f1-macro {f1_macro_init}')
+        print(f'Initial 30 classes f1-micro {f1_micro_init}', flush=True)
+
         return mAp, f1_macro, f1_macro, report
     
-def comparative_printing(class_report1: dict, class_report2: dict):
-    """
-    The function takes in 2 sklearn's classification reports. If the reports share the same key, then they are compared.
-    params:
-        class_report1: sklearn classification report dict
-        class_report2: sklearn classification report dict
-    return:
-        Nothing
-    """
-    for key1 in class_report1.keys():
-        for key2 in class_report2.keys():
-            if key1 == key2:
-                print(f"F1-score for class {key1}: \n new: {class_report1[key1]['f1-score']} \n old: {class_report2[key1]['f1-score']}")
 
 if __name__ == '__main__':
 
     # Command line args
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--nr_of_classes_new', type=int, choices=[30, 35, 40, 45, 50], help='Number of classes to use from data for new model. Choices [30, 35, 40, 45, 50]')
-    parser.add_argument('--nr_of_classes_old', type=int, choices=[30, 35, 40, 45, 50], help='Number of classes to use from data for old model. Choices [30, 35, 40, 45, 50]')
+    parser.add_argument('--nr_of_classes', type=int, choices=[30, 35, 40, 45, 50], help='Number of classes to use from data for new model. Choices [30, 35, 40, 45, 50]')
     parser.add_argument('--dataset', type=str, help='Choice of dataset. Either audioset or fsd50k')
     parser.add_argument('--path_to_data', type=str, help='The path to the HDF5 datafile.')
     parser.add_argument('--nr_of_workers', type=int, default=0, help='Number of workers for dataloading')
     parser.add_argument('--batch_size', type=int, default=8, help='Size of the loaded data batch. A tensor of [batch_size, data_tensor.shape] is loaded.')
     parser.add_argument('--path_to_model_state', type=str, help='Path to the pickle saved pytorch model state dict.')
-    parser.add_argument('--path_to_old_model_state', type=str, help='Path to the pickle saved pytorch model state dict of the older model.')
 
     args = vars(parser.parse_args())
 
@@ -84,49 +91,30 @@ if __name__ == '__main__':
     print(f"Using device: {device}")
 
     # Params
-    nr_of_classes_new = args['nr_of_classes_new']
-    nr_of_classes_old = args['nr_of_classes_old']
+    nr_of_classes = args['nr_of_classes_new']
     dataset = args['dataset']
     batch_size = args['batch_size']
     nr_of_workers = args['nr_of_workers']
     PATH_TO_HDF5_DATA = args['path_to_data'] #r'C:\Users\mp431591\Documents\work_code\cl_30\continual_learning\data_cl.hdf5'
     PATH_TO_MODEL_STATE = args['path_to_model_state']
-    PATH_TO_OLD_MODEL_STATE = args['path_to_old_model_state']
 
     # Data setup
-    data_eval_new = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
+    data_eval = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
                                     dataset=dataset,
                                     split='eval',
-                                    nr_of_classes=nr_of_classes_new)
-    data_eval_old = CL_dataset(path_to_data_hdf5=PATH_TO_HDF5_DATA,
-                                    dataset=dataset,
-                                    split='eval',
-                                    nr_of_classes=nr_of_classes_old)
+                                    nr_of_classes=nr_of_classes)
     
-    small_eval_data_new, _ = torch.utils.data.random_split(data_eval_new,
-                                                    [0.001, 0.999])
-    small_eval_data_old, _ = torch.utils.data.random_split(data_eval_old,
+    small_eval_data, _ = torch.utils.data.random_split(data_eval,
                                                     [0.001, 0.999])
     
-    evaluation_loader_new = torch.utils.data.DataLoader(small_eval_data_new, batch_size=batch_size, num_workers=nr_of_workers)
-    evaluation_loader_old = torch.utils.data.DataLoader(small_eval_data_old, batch_size=batch_size, num_workers=nr_of_workers)
+    evaluation_loader_new = torch.utils.data.DataLoader(small_eval_data, batch_size=batch_size, num_workers=nr_of_workers)
 
-
-    model_new = Cnn14(nr_of_classes_new)
+    model_new = Cnn14(nr_of_classes)
     model_new.load_state_dict(torch.load(PATH_TO_MODEL_STATE,
                                          weights_only=True))
-    model_old = Cnn14(nr_of_classes_old)
-    model_old.load_state_dict(torch.load(PATH_TO_OLD_MODEL_STATE,
-                                         weights_only=True))
 
-
-    mAP_new, f1_macro_new, f1_micro_new, report_new = evaluate(model=model_new,
+    mAP, f1_macro, f1_micro, report = evaluate(model=model_new,
                                                eval_loader=evaluation_loader_new)
-    mAP_old, f1_macro_old, f1_micro_old, report_old = evaluate(model=model_old,
-                                                               eval_loader=evaluation_loader_old)
-    print(f"New metrics:\n mAP {mAP_new}, f1_M {f1_macro_new}, f1_m {f1_micro_new}")
-    print(f"Old metrics:\n mAP {mAP_old}, f1_M {f1_macro_old}, f1_m {f1_micro_old}")
-    comparative_printing(report_new, report_old)
 
 
 
