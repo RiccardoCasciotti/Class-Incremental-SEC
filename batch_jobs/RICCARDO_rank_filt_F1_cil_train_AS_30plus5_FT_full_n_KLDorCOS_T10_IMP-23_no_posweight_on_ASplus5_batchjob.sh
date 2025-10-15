c54267047490663226757578653be5c60a433479
RiccardoCasciotti <riccardo.casciotti.1998@gmail.com>
2025-10-15 11:54:40 +0300

    batch job update

---
+++ batch_jobs/RICCARDO_rank_filt_F1_cil_train_AS_30plus5_FT_full_n_KLDorCOS_T10_IMP-23_no_posweight_on_ASplus5_batchjob.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH --job-name=rank_filt_cil_AS30plus5_on_ASplus5_w_KLD
+#SBATCH --account=project_462000765
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=7
+#SBATCH --mem=60G
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=small-g
+#SBATCH --output=/pfs/lustrep2/projappl/project_462000765/casciott/continual_learning/batch_jobs/outputs/rank_filt_F1_cil_train_AS30plus5_FT_full_n_KLDorCOS_T10_IMP-23_no_posweight_on_ASplus5/out.txt
+#SBATCH --error=/pfs/lustrep2/projappl/project_462000765/casciott/continual_learning/batch_jobs/outputs/rank_filt_F1_cil_train_AS30plus5_FT_full_n_KLDorCOS_T10_IMP-23_no_posweight_on_ASplus5/err.txt
+
+cd /pfs/lustrep2/projappl/project_462000765/casciott/continual_learning
+# Default modified convolutional layers are 7-12
+SCRIPT_NAME='rank_filt_cil_model_training_script.py'
+MODEL='audioset'
+
+EPOCHS=100
+NR_OF_CLASSES=30
+CIL_NR_OF_CLASSES=5
+DATASET='audioset'
+PATH_TO_DATA='/pfs/lustrep2/scratch/project_462000765/matias/data_cl_complete.hdf5'
+NR_OF_WORKERS=6
+BATCH_SIZE=32
+CHECKPOINT_INTERVAL=10
+LOG_INTERVAL=1
+T=10
+CLASS_IMPACT=-23
+FILTER_NR=1
+PATH_TO_MODEL_STATE="/pfs/lustrep2/scratch/project_462000765/matias/trained_models/trained_model_${MODEL}_${NR_OF_CLASSES}.pt"
+PATH_TO_COMPARISON_MODEL_STATE="/pfs/lustrep2/scratch/project_462000765/matias/trained_models/trained_model_${MODEL}_${NR_OF_CLASSES}.pt"
+filter_dir_name="$(basename $PATH_TO_MODEL_STATE .pt)"
+PATH_TO_FILTER_SCORE_DIR="/pfs/lustrep2/projappl/project_462000765/matias/scripts/outputs/PANNs_CNN14_filter_scores_per_layer/${filter_dir_name}/"
+MODEL_NAME="trained_rank_filt_F${FILTER_NR}_cil_model_${MODEL}_${NR_OF_CLASSES}plus${CIL_NR_OF_CLASSES}_FT_full_on_${DATASET}_${CIL_NR_OF_CLASSES}_n_KLDorCOS_T${T}_IMP${CLASS_IMPACT}_no_posweight_${EPOCHS}epochs.pt"
+
+# Desired destination folder of the trained model states
+MODEL_STATE_DEST='/pfs/lustrep2/projappl/project_462000765/casciott/continual_learning/'
+
+# Load the modules
+module use /appl/local/csc/modulefiles/
+module load pytorch/2.7
+
+# MIOPEN needs some initialisation for the cache as the default location
+# does not work on LUMI as Lustre does not provide the necessary features.
+export MIOPEN_USER_DB_PATH="/tmp/$(whoami)-miopen-cache-$SLURM_NODEID"
+export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
+
+srun python3 "$SCRIPT_NAME" \
+--epochs "$EPOCHS" \
+--nr_of_classes "$NR_OF_CLASSES" \
+--cil_nr_of_classes "$CIL_NR_OF_CLASSES" \
+--dataset "$DATASET" \
+--path_to_data "$PATH_TO_DATA" \
+--nr_of_workers "$NR_OF_WORKERS" \
+--batch_size "$BATCH_SIZE" \
+--checkpoint_interval "$CHECKPOINT_INTERVAL" \
+--log_interval "$LOG_INTERVAL" \
+--path_to_model_state "$PATH_TO_MODEL_STATE" \
+--path_to_comparison_model_state "$PATH_TO_COMPARISON_MODEL_STATE" \
+--model_name "$MODEL_NAME" \
+--T "$T" \
+--class_impact "$CLASS_IMPACT" \
+--filter_nr "$FILTER_NR" \
+--path_to_filter_score_dir "$PATH_TO_FILTER_SCORE_DIR" \
+--use_amp \
+--save_latest_epoch_model \
+--validate_w_map \
+--no_pos_weight \
+--weight_decay 0 \
+--model_state_dest "$MODEL_STATE_DEST"
